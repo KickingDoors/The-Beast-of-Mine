77164a5bd08bdceb0cdebb93368af99fbc42cc41
RafaelSPBarbosa <rafaspb11@gmail.com>
2016-07-22 12:48:27 -0300

    Verificar condições do Action Tracker

--- The Beast of Mine/Assets/Scripts/LoadChoices.cs
+++ The Beast of Mine/Assets/Scripts/LoadChoices.cs
@@ -3,10 +3,22 @@ using System.Collections;
 
 public class LoadChoices : MonoBehaviour {
 
-	public Choices TargetChoices;
+	public Choices TargetChoicesNoAction, TargetChoicesWithAction;
+	public int ActionID;
 
 	public void OpenChoices(){
 
-		TargetChoices.enabled = true;
+		if (ActionID != 0) {
+			if (GameObject.Find ("GameManager").GetComponent<ActionTracker> ().Variables [ActionID] == false) {
+
+				TargetChoicesNoAction.enabled = true;
+
+			} else {
+
+				TargetChoicesWithAction.enabled = true;
+			}
+		} else {
+			TargetChoicesNoAction.enabled = true;
+		}
 	}
 }
