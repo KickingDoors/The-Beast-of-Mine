ce797fa35e3a21eefb9a9956a5e8c94532e9545d
Rafaela Pontes Gestal <raafaela_pontes@hotmail.com>
2016-09-17 03:25:23 -0300

    QTE 1 - IMPLEMENTADO

--- The Beast of Mine/Assets/Scripts/GameManager.cs
+++ The Beast of Mine/Assets/Scripts/GameManager.cs
@@ -28,14 +28,37 @@ public class GameManager : MonoBehaviour {
         Time.timeScale = 1;
     }
 
+
     public void ChangeLanguage()
     {
         if (LanguageManager.Instance.LoadedLanguage == "pt-BR")
         {
             LanguageManager.Instance.ChangeLanguage("en-US");
+            PlayerPrefs.SetString("Language", "en-US");
             LanguageBtn_Txt.text = "EN";
         }
         else
+        {
+            LanguageManager.Instance.ChangeLanguage("pt-BR");
+            PlayerPrefs.SetString("Language", "pt-BR");
+            LanguageBtn_Txt.text = "PT";
+        }
+
+        PlayerPrefs.Save();
+    }
+
+
+    void Start()
+    {
+
+        string Lang = PlayerPrefs.GetString("Language");
+
+        if (Lang == "en-US")
+        {
+            LanguageManager.Instance.ChangeLanguage("en-US");
+            LanguageBtn_Txt.text = "EN";
+        }
+        else if (Lang == "pt-BR")
         {
             LanguageManager.Instance.ChangeLanguage("pt-BR");
             LanguageBtn_Txt.text = "PT";
