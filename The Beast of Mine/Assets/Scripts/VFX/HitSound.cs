38c7db94fba7d5a21a40694d0792a7e568e9264e
RafaelSPBarbosa <rafaspb11@gmail.com>
2017-02-10 17:08:53 -0200

    Dor e Coração SFX implementação

---
+++ The Beast of Mine/Assets/Scripts/VFX/HitSound.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HitSound : MonoBehaviour {
+
+	public AudioSource _AS;
+
+	public void Hit(){
+
+		_AS.clip = Resources.Load<AudioClip> ("Sounds/Dor " + Random.Range(1,4)) as AudioClip;
+		_AS.Play ();
+	}
+}
