f9db98687e468ca74aa4c3980e77443a0b9e90ba
RafaelSPBarbosa <rafaspb11@gmail.com>
2017-03-26 18:55:51 -0300

    Começo de Implementação de VFX + Som de Chuva

---
+++ The Beast of Mine/Assets/Scripts/VFX/HitVFX.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HitVFX : StateMachineBehaviour {
+
+    [SerializeField]
+    private int Health;
+    [SerializeField]
+    private bool isHit = true;
+
+    void OnStateEnter()
+    {
+        Animator anim = GameObject.Find("Hit VFX").GetComponent<Animator>();
+        anim.SetInteger("Health", Health);
+        if(isHit)
+            anim.SetTrigger("Hit");
+    }
+}
