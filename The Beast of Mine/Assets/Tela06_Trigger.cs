49ac53bf673e08a6cfffc51e8a3bc7ed586b9e6a
RafaelSPBarbosa <rafaspb11@gmail.com>
2017-03-28 20:30:15 -0300

    VFX Episódio 1

---
+++ The Beast of Mine/Assets/Tela06_Trigger.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tela06_Trigger : StateMachineBehaviour {
+
+
+    void OnStateEnter()
+    {
+        GameObject.Find("tela5").GetComponent<Animator>().SetTrigger("Avermelhado");
+	}
+}
